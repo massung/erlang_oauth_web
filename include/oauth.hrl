b4ec5d7974561e547154f14554d9f1cc9ccc9dc3
Jeffrey Massung <jeff@risrr.(none)>
2011-08-22 11:42:42 -0600

    Added type specification for valid signature methods.

--- include/oauth.hrl
+++ include/oauth.hrl
@@ -1,8 +1,14 @@
 %% oauth.hrl
 %%
 
+%% valid types of signature methods
+-type signature_method() :: 'PLAINTEXT' | 
+			    'HAMC-SHA1' | 
+			    'RSA-SHA1'.
+
+%% data given by the oauth provider
 -record(oauth_consumer,
 	{key,
 	 secret,
-	 signature_method='HMAC-SHA1'
+	 signature_method='HMAC-SHA1' :: signature_method()
 	}).
